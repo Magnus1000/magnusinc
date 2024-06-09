e51a01b4bf0e55f60306a9999c4827c82e319c66
Jack McDonald <jackmagnusmcdonald@gmail.com>
2024-06-09 15:41:05 -0400

    Updates

--- locationService.js
+++ locationService.js
@@ -2,6 +2,11 @@ const LocationService = () => {
     const [log, setLog] = React.useState('// event_logs');
 
     React.useEffect(() => {
+        let uuid = Cookies.get('uuid');
+        if (!uuid) {
+          uuid = generateUUID();
+          Cookies.set('uuid', uuid);
+        }
         fetch('https://ip-api.com/json')
         .then(response => response.json())
         .then(data => {
@@ -15,6 +20,22 @@ const LocationService = () => {
             setLog((prevLog) => `${prevLog}\n[${new Date().toISOString()}] Error getting location via IP: ${error.message}`);
         });
     }, []);
+
+    function generateUUID() { // Public Domain/MIT
+        var d = new Date().getTime();//Timestamp
+        var d2 = (performance && performance.now && (performance.now()*1000)) || 0;//Time in microseconds since page-load or 0 if unsupported
+        return 'xxxxxxxx-xxxx-4xxx-yxxx-xxxxxxxxxxxx'.replace(/[xy]/g, function(c) {
+            var r = Math.random() * 16;//random number between 0 and 16
+            if(d > 0){//Use timestamp until depleted
+                r = (d + r)%16 | 0;
+                d = Math.floor(d/16);
+            } else {//Use microseconds since page-load if supported
+                r = (d2 + r)%16 | 0;
+                d2 = Math.floor(d2/16);
+            }
+            return (c === 'x' ? r : (r & 0x3 | 0x8)).toString(16);
+        });
+    }
 
     const handleGetLocation = () => {
         setLog((prevLog) => `${prevLog}\n[${new Date().toISOString()}] Button clicked.`);
