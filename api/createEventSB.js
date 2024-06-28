e2853fbcd5cfc90aa9feec001ab3906398b8171f
Jack McDonald <jackmagnusmcdonald@gmail.com>
2024-06-28 11:25:45 -0500

    Updates

--- api/createEventSB.js
+++ api/createEventSB.js
@@ -9,7 +9,7 @@ const corsHandler = cors();
 module.exports = async (req, res) => {
     corsHandler(req, res, async () => {
         // Initialize Supabase client
-        const supabase = createClient(process.env.SUPABASE_URL, process.env.SUPABASE_ANON_KEY);
+        const supabase = createClient(process.env.SUPABASE_URL, process.env.SUPABASE_KEY);
 
         // Extract parameters from request body
         const { uuid, event_content, event_type, event_page } = req.body;
