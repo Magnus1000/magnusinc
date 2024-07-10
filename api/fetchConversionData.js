e8e717159c476ae9981957a9e19716de960a3f7b
Jack McDonald <jackmagnusmcdonald@gmail.com>
2024-07-10 10:33:16 -0400

    Updates

--- api/fetchConversionData.js
+++ api/fetchConversionData.js
@@ -1,28 +1,34 @@
 // /api/fetchClosestResults.js
-const axios = require('axios');
 const cors = require('cors')();
+const { createClient } = require('@supabase/supabase-js');
+
+const supabaseUrl = 'https://lbrtnalayoyzwrnthdse.supabase.co';
+const supabaseKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImxicnRuYWxheW95endybnRoZHNlIiwicm9sZSI6ImFub24iLCJpYXQiOjE3MTk1ODgyMjEsImV4cCI6MjAzNTE2NDIyMX0.H16NPoL9OS-7l_GoaoJ-2xKQZ-CdJ4Mo9QXpM-6YRfY';
+const supabase = createClient(supabaseUrl, supabaseKey);
 
 module.exports = (req, res) => {
   cors(req, res, async () => {
     try {
-        const response = await axios.get(`https://api.airtable.com/v0/${process.env.AIRTABLE_APP_ID}/${process.env.AIRTABLE_EVENT_TYPE_TABLE_ID}`, {
-            headers: {
-            'Authorization': `Bearer ${process.env.AIRTABLE_API_KEY}`
-            }
-        });
+      const { data, error } = await supabase
+        .from('event_logs') // Replace with your actual table name
+        .select('event_type');
 
-        console.log(`Received response from Airtable: ${JSON.stringify(response.data)}`); // Add this line
+      if (error) {
+        throw error;
+      }
 
-        const records = response.data.records.map(record => ({
-            event_type: record.fields.event_type,
-            event_order: record.fields.event_order,
-            event_count: record.fields.event_count,
-        }));
+      console.log(`Received response from Supabase: ${JSON.stringify(data)}`);
 
-        res.json(records);
+      // Aggregate the data by event_type
+      const eventTypeCounts = data.reduce((acc, record) => {
+        acc[record.event_type] = (acc[record.event_type] || 0) + 1;
+        return acc;
+      }, {});
+
+      res.json(eventTypeCounts);
     } catch (error) {
-        console.error(`An error occurred: ${error.message}`); // Add this line
-        res.status(500).json({ error: 'An error occurred while fetching data from Airtable' });
+      console.error(`An error occurred: ${error.message}`);
+      res.status(500).json({ error: 'An error occurred while fetching data from Supabase' });
     }
   });
 };
