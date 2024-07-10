ee895b3eb0ae64e818f2b4ef5d87209724814ab1
Jack McDonald <jackmagnusmcdonald@gmail.com>
2024-07-10 12:56:05 -0400

    Updates

--- chatbotClient.js
+++ chatbotClient.js
@@ -8,7 +8,7 @@ const Chatbot = () => {
       setInput('');
   
       try {
-        const response = await axios.post('/api/chatbot', {
+        const response = await axios.post('https://magnusinc-magnus1000team.vercel.app/api/chatbot', {
           input,
           messages,
         });
