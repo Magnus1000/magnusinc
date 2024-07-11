f260ce335341ac2e15e7862e8206ef32642dd00f
Jack McDonald <jackmagnusmcdonald@gmail.com>
2024-07-11 10:30:36 -0400

    Updates

---
+++ api/makeBooking.js
@@ -0,0 +1,22 @@
+import axios from 'axios';
+import cors from 'cors';
+
+module.exports = (req, res) => {
+  cors(req, res, async () => {
+	const { website, email, selectedServices, selectedBookingSlot } = req.body;
+
+	try {
+	  const response = await axios.post('https://hook.us1.make.com/y6c61clds14yvapchy7y8lfd5f0m4t8u', {
+		website,
+		email,
+		selectedServices,
+		selectedBookingSlot,
+	  });
+	  console.log('Booking submitted successfully:', response.data);
+	  res.status(200).send('Booking submitted successfully');
+	} catch (error) {
+	  console.error('Error submitting booking:', error);
+	  res.status(500).send('Error submitting booking');
+	}
+  });
+};
