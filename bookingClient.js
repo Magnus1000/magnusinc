d8c6aa3433e74014eceedc00a5d1d9f491b39615
Jack McDonald <jackmagnusmcdonald@gmail.com>
2024-07-12 19:17:41 -0400

    u#

--- bookingClient.js
+++ bookingClient.js
@@ -89,7 +89,6 @@ const Booking = () => {
       });
       console.log('Booking submitted successfully:', response.data);
       createEvent('Booking submitted', 'booking_submitted');
-      // Delay setting bookingConfirmed to true by 1 second
       setTimeout(() => {
         setBookingConfirmed(true);
         setIsSubmitting(false);
@@ -156,7 +155,6 @@ const Booking = () => {
     const value = e.target.value;
     setWebsite(value);
     
-    // Simple validation
     if (value && !value.includes('.')) {
       setWebsiteError('Please enter a valid domain (e.g., google.com)');
     } else {
@@ -231,9 +229,21 @@ const Booking = () => {
                 <button 
                   className="confirm-button" 
                   onClick={fetchScreenshot}
-                  disabled={!website || websiteError}
+                  disabled={!website || websiteError || screenshotLoading}
                 >
-                  Submit
+                  <div className="button-icon">
+                    {screenshotLoading ? (
+                      <>
+                        Analyzing
+                        <svg className="spinning" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 512 512">
+                          <path fill="currentColor" opacity=".4" d="M256 64C150 64 64 150 64 256s86 192 192 192c70.1 0 131.3-37.5 164.9-93.6l.1 .1c-6.9 14.9-1.5 32.8 13 41.2c15.3 8.9 34.9 3.6 43.7-11.7c.2-.3 .4-.6 .5-.9l0 0C434.1 460.1 351.1 512 256 512C114.6 512 0 397.4 0 256S114.6 0 256 0c-17.7 0-32 14.3-32 32s14.3 32 32 32z"/>
+                          <path fill="currentColor" d="M224 32c0-17.7 14.3-32 32-32C397.4 0 512 114.6 512 256c0 46.6-12.5 90.4-34.3 128c-8.8 15.3-28.4 20.5-43.7 11.7s-20.5-28.4-11.7-43.7c16.3-28.2 25.7-61 25.7-96c0-106-86-192-192-192c-17.7 0-32-14.3-32-32z"/>
+                        </svg>
+                      </>
+                    ) : (
+                      'Submit'
+                    )}
+                  </div>
                 </button>
               </div>
               {websiteError && <p className="error-message">{websiteError}</p>}
