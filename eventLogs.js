51f8fd9f46752c1149b32ca2c58cf47e215fef0c
Jack McDonald <jackmagnusmcdonald@gmail.com>
2024-07-18 17:05:21 -0400

    Updates

--- eventLogs.js
+++ eventLogs.js
@@ -2,7 +2,6 @@ function EventLogs() {
   const [logs, setLogs] = React.useState([]);
   const [uuid, setUuid] = React.useState('');
   const [supabase, setSupabase] = React.useState(null);
-  const [emailLogsHTML, setEmailLogsHTML] = React.useState(''); // Added state for email logs HTML
 
   React.useEffect(() => {
     const initializeSupabase = () => {
@@ -94,7 +93,7 @@ function EventLogs() {
     return () => {
       supabase.removeSubscription(subscription);
     };
-  }, [supabase, uuid]);
+  }, [supabase]);
 
   React.useEffect(() => {
     const getUuidFromCookies = () => {
@@ -126,19 +125,20 @@ function EventLogs() {
       const formattedData = data.map((log, index) => {
         const { event_id, event_time, event_type, event_page } = log;
         return `
-<div class="event-log uuid">
-  <span class="code-line-number">${index + 1}</span>
-  <span class="lime-green">${JSON.stringify({ event_id, event_time: new Date(event_time).toISOString().split('.')[0], event_type, event_page })}</span>
-</div>`;
+  <div class="event-log uuid">
+    <span class="code-line-number">${index + 1}</span>
+    <span class="lime-green">${JSON.stringify({ event_id, event_time: new Date(event_time).toISOString().split('.')[0], event_type, event_page })}</span>
+  </div>`;
       }).join('');
   
       setEmailLogsHTML(formattedData);
     }
-  };
+  };  
+
 
   React.useEffect(() => {
     fetchEmailCaptureLogs();
-  }, [supabase, uuid]);
+  }, [supabase]);
 
   return (
     <div className="service-row">
